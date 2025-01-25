5a1a2b720f5ae0a4f39b95ee066f945ceaf38224
Genbots <72676328+GenbotsVRC@users.noreply.github.com>
2025-01-25 07:15:49 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,31 @@
-def hi():
-  pass
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       1053730                                                      #
+# 	Created:      1/25/2025, 7:13:27 AM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+brain = Brain()
+
+def autonomous():
+    brain.screen.clear_screen()
+    brain.screen.print("autonomous code")
+    # place automonous code here
+
+def user_control():
+    brain.screen.clear_screen()
+    brain.screen.print("driver control")
+    # place driver control in this while loop
+    while True:
+        wait(20, MSEC)
+
+# create competition instance
+comp = Competition(user_control, autonomous)
+
+# actions to do when the program starts
+brain.screen.clear_screen()
