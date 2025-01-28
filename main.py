d1c5314589f473b4bb75c5830ed2346da3a288c5
Shdwy <raphaelquarles@gmail.com>
2025-01-28 15:40:48 -0500

    started sorting code, adding comments, and declaring functions
    format probably works now

--- main.py
+++ main.py
@@ -17,34 +17,48 @@ controller = Controller()
 clamp = Motor(Ports.PORT10)
 intake = Motor(Ports.PORT11)
 train = Motor(Ports.PORT6)
-left_motor_a = Motor(Ports.PORT8)
-left_motor_b = Motor(Ports.PORT9)
-left_drive_smart = MotorGroup(left_motor_a, left_motor_b)
-right_motor_a = Motor(Ports.PORT20)
-right_motor_b = Motor(Ports.PORT19)
-right_drive_smart = MotorGroup(right_motor_a, right_motor_b)
-drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+leftA = Motor(Ports.PORT8)
+leftB = Motor(Ports.PORT9)
+left = MotorGroup(leftA, leftB)
+rightA = Motor(Ports.PORT20)
+rightB = Motor(Ports.PORT19)
+right = MotorGroup(rightA, rightB)
+drivetrain = DriveTrain(left, right, 319.19, 295, 40, MM, 1)
 
 # initalize 
 wait(30, MSEC)
 
-
-
-def autonomous():
+############################## AUTON (PARTITION) ##############################
+def auton():
     brain.screen.clear_screen()
     brain.screen.print("autonomous code")
     # place automonous code here
 
+############################## MOTOR FUNCTIONS ##############################
+def testing():
+    controller.rumble(".")
+    brain.screen.print("ihe")
+
+############################## CALLBACKS ##############################
+controller.buttonA.pressed(testing)
+
 def user_control():
     brain.screen.clear_screen()
     brain.screen.print("driver control")
-    # place driver control in this while loop
+
+    
+    
+
+
+    # place something in while loop (unclear)
+    # i believe drive settings
     while True:
         wait(20, MSEC)
+        
 
 # create competition instance
-comp = Competition(user_control, autonomous)
+comp = Competition(user_control, auton)
 
 # actions to do when the program starts
-brain.screen.clear_screen()
+
 controller.rumble("...")
