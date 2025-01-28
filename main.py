72f482900ee39b9d3ebf9def7ab37c9ed3778843
Shdwy <raphaelquarles@gmail.com>
2025-01-28 07:18:50 -0500

    added ports and rumble initialization

--- main.py
+++ main.py
@@ -12,6 +12,24 @@ from vex import *
 
 brain = Brain()
 
+controller = Controller()
+
+clamp = Motor(Ports.PORT10)
+intake = Motor(Ports.PORT11)
+train = Motor(Ports.PORT6)
+left_motor_a = Motor(Ports.PORT8)
+left_motor_b = Motor(Ports.PORT9)
+left_drive_smart = MotorGroup(left_motor_a, left_motor_b)
+right_motor_a = Motor(Ports.PORT20)
+right_motor_b = Motor(Ports.PORT19)
+right_drive_smart = MotorGroup(right_motor_a, right_motor_b)
+drivetrain = DriveTrain(left_drive_smart, right_drive_smart, 319.19, 295, 40, MM, 1)
+
+# initalize 
+wait(30, MSEC)
+
+
+
 def autonomous():
     brain.screen.clear_screen()
     brain.screen.print("autonomous code")
@@ -29,3 +47,4 @@ comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
 brain.screen.clear_screen()
+controller.rumble("...")
